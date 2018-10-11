de724b527624a1549996892607d3e5a7790672cd
futurouz <kunanan.tas@mail.kmutt.ac.th>
2018-10-11 21:26:28 +0700

    Add Input component

---
+++ src/client/components/UI/Input/Input.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Input = props => {
+  let inputElement = null;
+
+  switch (props.elementType) {
+    case 'input':
+      inputElement = <input {...props.elementConfig} value={props.value} />;
+      break;
+    case 'select':
+      inputElement = (
+        <select value={props.value} onChange={props.changed}>
+          {props.elementConfig.options.map(option => (
+            <option key={option.value} value={option.value}>
+              {option.displayValue}
+            </option>
+          ))}
+        </select>
+      );
+      break;
+    default:
+      inputElement = <input {...props.elementConfig} value={props.value} />;
+  }
+
+  return (
+    <div>
+      <label>{props.label}</label>
+      {inputElement}
+    </div>
+  );
+};
+
+export default Input;
