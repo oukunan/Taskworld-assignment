ea8cf597d34bb9532484510d3a0cc228b7ea12ad
futurouz <kunanan.tas@mail.kmutt.ac.th>
2018-10-12 21:59:41 +0700

    Add form and summit data]

--- src/client/containers/Preference/Preference.js
+++ src/client/containers/Preference/Preference.js
@@ -1,65 +1,148 @@
 import React, { Component } from 'react';
 import Input from '../../components/UI/Input/Input';
-
+import Button from '../../components/UI/Button/Button';
 class Preference extends Component {
   state = {
-    preferenceForm: {
-      Language: {
-        elementType: 'select',
-        elementConfig: {
-          options: [
-            { value: 'English', displayValue: 'English' },
-            { value: 'Thai', displayValue: 'Thai' }
-          ]
-        },
-        value: 'English'
-      },
-      'Time Zone': {
-        elementType: 'select',
-        elementConfig: {
-          options: [
-            { value: 'English', displayValue: 'English' },
-            { value: 'Thai', displayValue: 'Thai' }
-          ]
-        },
-        value: 'English'
-      },
-      Currency: {
-        elementType: 'select',
-        elementConfig: {
-          options: [
-            { value: 'USD', displayValue: 'USD' },
-            { value: 'THB', displayValue: 'THB' }
-          ]
-        },
-        value: 'English'
-      }
-    }
+    language: 'English',
+    timezone: '(+00:00)UTC',
+    currency: 'USD',
+    visibility: 'Everyone',
+    message: 'Everyone',
+    category: 'Enable'
   };
+
+  inputChangedHandler = e => {
+    let name = e.target.name;
+    let value = e.target.value;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  savePreferenceHandler = e => {
+    e.preventDefault();
+    console.log(this.state);
+  };
+
   render() {
-    const formArray = [];
-    for (let key in this.state.preferenceForm) {
-      formArray.push({
-        id: key,
-        config: this.state.preferenceForm[key]
-      });
-    }
+    return (
+      <div>
+        <form onSubmit={this.savePreferenceHandler}>
+          <div>
+            <h3>Edit preference</h3>
+            <h5>Localization</h5>
+            <label>Language</label>
+            <select
+              onChange={this.inputChangedHandler}
+              name="language"
+              value={this.state.language}
+            >
+              <option value="English">English</option>
+              <option value="Thai">Thai</option>
+            </select>
 
-    let form = (
-      <form>
-        {formArray.map(formElement => (
-          <Input
-            key={formElement.id}
-            elementType={formElement.config.elementType}
-            elementConfig={formElement.config.elementConfig}
-            value={formElement.config.value}
-            label={formElement.id}
-          />
-        ))}
-      </form>
+            <label>Time zone</label>
+            <select
+              onChange={this.inputChangedHandler}
+              name="timezone"
+              value={this.state.timezone}
+            >
+              <option value="(+00:00)UTC">(+00:00)UTC</option>
+              <option value="(+01:00)UTC">(+01:00)UTC</option>
+            </select>
+
+            <label>Currency</label>
+            <select
+              onChange={this.inputChangedHandler}
+              name="currency"
+              value={this.state.currency}
+            >
+              <option value="USD">USD</option>
+              <option value="THB">THB</option>
+            </select>
+          </div>
+          <div>
+            <h5>Privacy</h5>
+            <div>
+              <label>Profile visibility</label>
+              <input
+                type="radio"
+                name="visibility"
+                value="Everyone"
+                checked={this.state.visibility === 'Everyone'}
+                onChange={this.inputChangedHandler}
+              />{' '}
+              Everyone
+              <input
+                type="radio"
+                name="visibility"
+                value="Private"
+                checked={this.state.visibility === 'Private'}
+                onChange={this.inputChangedHandler}
+              />{' '}
+              Private
+            </div>
+            <br />
+            <div>
+              <label>Messages</label>
+              <input
+                type="radio"
+                name="message"
+                value="Everyone"
+                checked={this.state.message === 'Everyone'}
+                onChange={this.inputChangedHandler}
+              />{' '}
+              Everyone
+              <input
+                type="radio"
+                name="message"
+                value="People you follow"
+                checked={this.state.message === 'People you follow'}
+                onChange={this.inputChangedHandler}
+              />{' '}
+              people you follow
+              <input
+                type="radio"
+                name="message"
+                value="No one"
+                checked={this.state.message === 'No one'}
+                onChange={this.inputChangedHandler}
+              />{' '}
+              No one
+            </div>
+            <br />
+
+            <div>
+              <label>Recently viewed</label>
+              <Button>Delete all items</Button>
+            </div>
+          </div>
+          <div>
+            <h5>Content</h5>
+            <div>
+              <label>Category lists</label>
+              <input
+                type="radio"
+                name="category"
+                value="Enable"
+                checked={this.state.category === 'Enable'}
+                onChange={this.inputChangedHandler}
+              />{' '}
+              Enable
+              <input
+                type="radio"
+                name="category"
+                value="Disable"
+                checked={this.state.category === 'Disable'}
+                onChange={this.inputChangedHandler}
+              />{' '}
+              Disable
+            </div>
+          </div>
+          <Button>Save preference</Button>
+        </form>
+      </div>
     );
-
-    return <div>{form}</div>;
   }
 }
 
