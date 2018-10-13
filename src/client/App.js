600580085e82a4111a5083ef3f3c7851972a656f
futurouz <kunanan.tas@mail.kmutt.ac.th>
2018-10-13 20:05:38 +0700

    Add login comtainer

--- src/client/App.js
+++ src/client/App.js
@@ -5,13 +5,15 @@ import './app.css';
 import Layout from './hoc/Layout';
 import Preference from './containers/Preference/Preference';
 import Profile from './containers/Profile/Profile';
+import Login from './containers/Login/Login';
 class App extends Component {
   render() {
     return (
       <Layout>
         <Switch>
           <Route path="/profile" component={Profile} />
-          <Route exact path="/" component={Preference} />
+          <Route path="/preference" component={Preference} />
+          <Route exact path="/" component={Login} />
         </Switch>
       </Layout>
     );
---
+++ src/client/containers/Login/Login.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+import Button from '../../components/UI/Button/Button';
+
+class Login extends Component {
+  state = {
+    email: '',
+    password: ''
+  };
+
+  loginHandler = e => {
+    e.preventDefault();
+    axios
+      .post('http://localhost:8080/user', this.state)
+      .then(res => {
+        console.log(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  handleInput = e => {
+    const name = e.target.name;
+    const value = e.target.value;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.loginHandler}>
+          <input type="email" name="email" onChange={this.handleInput} />
+          <input type="password" name="password" onChange={this.handleInput} />
+          <Button>Login</Button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Login;
