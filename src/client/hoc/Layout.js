8f6f594c0014970f8ca89fcefe61c2b775c1852e
futurouz <kunanan.tas@mail.kmutt.ac.th>
2018-10-14 20:25:27 +0700

    Add handler UI auth state

--- src/client/hoc/Layout.js
+++ src/client/hoc/Layout.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
 import Navbar from '../components/Navbar/Navbar';
 import Menu from '../components/Menu/Menu';
+import { AuthConsumer } from '../context/AuthContext';
+
 class Layout extends Component {
   render() {
     return (
       <div>
         <Navbar />
+
         <div className="container-layout">
-          <div className="sub-layout menu-layout">
-            <Menu />
-          </div>
+          {this.props.isAuth ? (
+            <div className="sub-layout menu-layout">
+              <Menu />
+            </div>
+          ) : null}
           <div className="sub-layout child-layout">{this.props.children}</div>
         </div>
       </div>
@@ -17,4 +22,10 @@ class Layout extends Component {
   }
 }
 
-export default Layout;
+export default props => (
+  <AuthConsumer>
+    {({ isAuth }) => {
+      return <Layout {...props} isAuth={isAuth} />;
+    }}
+  </AuthConsumer>
+);
