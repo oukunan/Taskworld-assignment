8e602540df468cec8c0f501424480bb508979b6a
futurouz <kunanan.tas@mail.kmutt.ac.th>
2018-10-16 22:52:37 +0700

    Remove AuthConsumer from navbar

--- src/client/components/Navbar/Navbar.js
+++ src/client/components/Navbar/Navbar.js
@@ -1,40 +1,32 @@
 import React from 'react';
 
-import NavbarItem from './NavbarItem/NavbarItem';
 import NavbarSection from './NavbarSection/NavbarSection';
-import { AuthConsumer } from '../../context/AuthContext';
 const navbar = props => (
   <div className="navbar">
     <ul>
-      <AuthConsumer>
-        {({ isAuth, logout }) => (
-          <div className="navbar-menu">
-            {isAuth ? (
-              <NavbarSection>
-                <input type="text" placeholder="Search Fancy" id="search" />
-              </NavbarSection>
-            ) : null}
-            <NavbarSection>
-              <div className="logo">
-                FANCY
-              </div>
-            </NavbarSection>
+      <div className="navbar-menu">
+        {props.isAuth ? (
+          <NavbarSection>
+            <input type="text" placeholder="Search Fancy" id="search" />
+          </NavbarSection>
+        ) : null}
+        <NavbarSection>
+          <div className="logo">FANCY</div>
+        </NavbarSection>
 
-            {isAuth ? (
-              <NavbarSection>
-                <div className="dropdown">
-                  <button className="dropbtn">
-                    You <i id="arrow" className="down" />
-                  </button>
-                  <div className="dropdown-content">
-                    <div onClick={logout}>Logout?</div>
-                  </div>
-                </div>
-              </NavbarSection>
-            ) : null}
-          </div>
-        )}
-      </AuthConsumer>
+        {props.isAuth ? (
+          <NavbarSection>
+            <div className="dropdown">
+              <button className="dropbtn">
+                You <i id="arrow" className="down" />
+              </button>
+              <div className="dropdown-content">
+                <div onClick={props.logout}>Logout?</div>
+              </div>
+            </div>
+          </NavbarSection>
+        ) : null}
+      </div>
     </ul>
   </div>
 );
--- src/client/hoc/Layout.js
+++ src/client/hoc/Layout.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
+
 import Navbar from '../components/Navbar/Navbar';
 import Menu from '../components/Menu/Menu';
+import Right from '../components/Right/Right';
 import { AuthConsumer } from '../context/AuthContext';
 
 class Layout extends Component {
   render() {
     return (
       <div className="layout">
-        <Navbar />
+        <Navbar isAuth={this.props.isAuth} logout={this.props.logout} />
         <div className="container-layout">
           {this.props.isAuth ? (
             <div className="sub-layout">
@@ -23,8 +25,16 @@ class Layout extends Component {
 
 export default props => (
   <AuthConsumer>
-    {({ isAuth }) => {
-      return <Layout {...props} isAuth={isAuth} />;
+    {({ isAuth, uid, login, logout }) => {
+      return (
+        <Layout
+          {...props}
+          isAuth={isAuth}
+          logout={logout}
+          login={login}
+          uid={uid}
+        />
+      );
     }}
   </AuthConsumer>
 );
