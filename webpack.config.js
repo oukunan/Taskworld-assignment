e42c016edfd29ecb6ec185785f5a7fa937bd137c
futurouz <kunanan.tas@mail.kmutt.ac.th>
2018-10-19 00:37:42 +0700

    Fixed client get cant get route

--- webpack.config.js
+++ webpack.config.js
@@ -34,7 +34,8 @@ module.exports = {
     open: true,
     proxy: {
       '/api': 'http://localhost:8080'
-    }
+    },
+    historyApiFallback: true
   },
   plugins: [
     new CleanWebpackPlugin([outputDirectory]),
