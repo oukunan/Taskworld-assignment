e02a7f1a20f87ea810d7e7b50813d2af85407799
futurouz <kunanan.tas@mail.kmutt.ac.th>
2018-10-19 23:18:43 +0700

    Loop route instead of repeat

--- src/client/App.js
+++ src/client/App.js
@@ -6,11 +6,27 @@ import Layout from './hoc/Layout';
 import Preference from './containers/Preference/Preference';
 import Signup from './containers/Signup/Signup';
 import ProtectedRoute from './context/ProtectedRoute';
+
+const routeList = [
+  { path: '/profile' },
+  { path: '/password' },
+  { path: '/notification' },
+  { path: '/connected' },
+  { path: '/order' },
+  { path: '/payment' },
+  { path: '/shipping' },
+  { path: '/credits' }
+];
 class App extends Component {
   render() {
+    let routes = null;
+    routes = routeList.map((route, i) => (
+      <ProtectedRoute key={i} path={route.path} component={() => null} />
+    ));
     return (
       <Layout>
         <Switch>
+          {routes}
           <ProtectedRoute path="/preference" component={Preference} />
           <Route exact path="/" component={Signup} />
         </Switch>
